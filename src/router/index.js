03dab6be1f19ae6b29c1efc925a1bc03573af59d
leebokyeong <kyeong8139@gmail.com>
2024-05-29 12:24:43 +0900

    router path modify

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path : "/portfolio",
+      path : "/",
       name : 'main',
       component : MainPageView
     },
